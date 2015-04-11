aa5c2bc5fa2164610245feb6e6690d63663fcc00
vahurh <vahurh@live.com>
2015-04-11 09:42:51 +0300

    Added data to bootstrap

--- paydork/grails-app/conf/BootStrap.groovy
+++ paydork/grails-app/conf/BootStrap.groovy
@@ -27,9 +27,21 @@ class BootStrap {
 		def cause = new Cause(name: "Bear observation in Tartu", description: "We leave at 9pm and WILL HAVE A NICE TIME! You will also bring cookies, two doughnuts and a dog. I want everyone to be dressed properly and wear a bow-tie. Please don't bring any cars nor big red popsicles.", 
 			schoolClass: schoolClass, image: "bear.jpg", price: 200, pricePerStudent: 20, tripDate: new Date(), 
 			dueDate: new Date()).save(failOnError: true)
+			
+		def cause2 = new Cause(name: "Tuhala nõiakaev", description: "We leave at 9pm and WILL HAVE A NICE TIME! You will also bring cookies, two doughnuts and a dog. I want everyone to be dressed properly and wear a bow-tie. Please don't bring any cars nor big red popsicles.",
+			schoolClass: schoolClass, image: "waterfall.jpg", price: 900, pricePerStudent: 10, tripDate: new Date(),
+			dueDate: new Date()).save(failOnError: true)
+			
+		def cause3 = new Cause(name: "Skiing in Himos", description: "We leave at 9pm and WILL HAVE A NICE TIME! You will also bring cookies, two doughnuts and a dog. I want everyone to be dressed properly and wear a bow-tie. Please don't bring any cars nor big red popsicles.",
+			schoolClass: schoolClass, image: "ski.jpg", price: 50, pricePerStudent: 5, tripDate: new Date(),
+			dueDate: new Date()).save(failOnError: true)
 		
 		cause.addToStudents(student)
 		cause.addToStudents(student2)
+		cause2.addToStudents(student)
+		cause2.addToStudents(student2)
+		cause3.addToStudents(student)
+		cause3.addToStudents(student2)
 		cause.addToEditors(editor)
 		
     }
